d612f476873b543de9188b7641dc9b65dd6e04f7
Usman <usman.mughal@outlook.fr>
2024-01-29 12:21:45 +0100

    Màj de la Modal

--- src/Lib/Components/Modal.jsx
+++ src/Lib/Components/Modal.jsx
@@ -1,38 +1,30 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Modal.css';
 
-const Modal = () => {
-  const [openModal, setOpenModal] = useState(false);
+const Modal = ({ title = "", text = "", closeModal }) => {
 
   return (
-    <div>
-      <button className="openModalBtn" onClick={() => setOpenModal(true)}>
-        Save
-      </button>
-      {openModal && (
-        <div className="modal__background">
-          <div className="modal__container">
-            <div className='titleCloseBtn'>
-              <button onClick={() => setOpenModal(false)}>X</button>
-            </div>
-            <div className="modal__title">Employee created</div>
-            <div className="modal__body">
-              <p>Your employee has been successfully created!</p>
-            </div>
-            <div className="modal__footer">
-              <button onClick={() => setOpenModal(false)}>Cancel</button>
-            </div>
-          </div>
+    <div className="modal__background">
+      <div className="modal__container">
+        <div className='titleCloseBtn'>
+          <button onClick={() => closeModal()}>X</button>
         </div>
-      )}
+        <div className="modal__title">{title}</div>
+        <div className="modal__body">
+          <p>{text}</p>
+        </div>
+        <div className="modal__footer">
+          <button onClick={() => closeModal()}>Cancel</button>
+        </div>
+      </div>
     </div>
   );
 };
 
 Modal.propTypes = {
   closeModal: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default Modal;
